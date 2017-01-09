8c7c9ba16796afd7a9077fa8ce53681970742986
caishijian2008 <442754043@qq.com>
2017-01-10 03:15:26 +0800

    添加修改了css和js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
-$(document).ready(function() {
+(function($) {
   
   /*  */
-  
+  let $window = $(window),
+      $body = $('body'),
+      $header = $('#header'),
+      $footer = $('#footer');
+
+  $body.addClass('is-loading');
+  $window.on('load', function() {
+    window.setTimeout(function() {
+      $body.removeClass('is-loading');
+    }, 100);
+  });
+
+  //Header
+  $header.each(function() {
+    let t = jQuery(this),
+        button = t.find('.button');
+    button.click(function(event) {
+      t.toggleClass('hide');
+      if (t.hasClass('preview')) {
+        return true;
+      } else {
+        event.preventDefault();
+      }
+    });
+  });
+
+
 
   /* scroll to top */
-  $(".fixed-btn").hide();
+  $(".gototop").hide();
   $(function() {
 	  $(window).scroll(function(e) {
 	    if($(window).scrollTop() >= 100) {
-	      $(".fixed-btn").fadeIn();
+	      $(".gototop").fadeIn();
 	    } else {
-	      $(".fixed-btn").fadeOut();
+	      $(".gototop").fadeOut();
 	    }
 	  });
   });
-  $(".fixed-btn").click(function(e) {
+  $(".gototop").click(function(e) {
     $("html, body").animate({scrollTop: 0}, 300); 
     return false; 
   }); 
 
-});
+})(jQuery);

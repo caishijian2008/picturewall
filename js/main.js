360330d393ce34cba5aad627ae22c5a9409bac8c
caishijian2008 <442754043@qq.com>
2017-01-16 12:20:20 +0800

    主页css特效完成！

--- js/main.js
+++ js/main.js
@@ -1,30 +1,52 @@
 (function($) {
   
-  /*  */
-  let $window = $(window),
-      $body = $('body'),
-      $header = $('#header'),
-      $footer = $('#footer');
-
-  $body.addClass('is-loading');
-  $window.on('load', function() {
-    window.setTimeout(function() {
-      $body.removeClass('is-loading');
-    }, 100);
+  skel.breakpoints({
+    xlarge: '(max-width: 1680px)',
+    large:  '(max-width: 1280px)',
+    medium: '(max-width: 980px)',
+    small:  '(max-width: 736px)',
+    xsmall: '(max-width: 480px)'
   });
 
-  //Header
-  $header.each(function() {
-    let t = jQuery(this),
-        button = t.find('.button');
-    button.click(function(event) {
-      t.toggleClass('hide');
-      if (t.hasClass('preview')) {
-        return true;
-      } else {
-        event.preventDefault();
-      }
+  /*  */
+  $(function() {
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header'),
+        $footer = $('#footer');
+
+    $body.addClass('is-loading');
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-loading');
+      }, 100);
     });
+
+    //Header
+    $header.each(function() {
+      var t = jQuery(this),
+          button = t.find('.button');
+      button.click(function(event) {
+        t.toggleClass('hide');
+        if (t.hasClass('preview')) {
+          return true;
+        } else {
+          event.preventDefault();
+        }
+      });
+    });
+
+    //Footer
+    $footer.each(function() {
+      var t = jQuery(this),
+          inner = t.find('.inner'),
+          info = t.find('.info');
+      info.click(function(event) {
+        t.toggleClass('show');
+        event.preventDefault();
+      });
+    });
+
   });
 
 
